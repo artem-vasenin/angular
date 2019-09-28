53ab1576b5736471c16534849fcf97ce173e5023
Rusich <artem.skobar@yandex.ru>
2019-09-28 17:37:37 +0300

    add admin login form

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,11 +6,14 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: '',
@@ -29,5 +32,4 @@ import { EditPageComponent } from './edit-page/edit-page.component';
 })
 
 export class AdminModule {
-  
 }
