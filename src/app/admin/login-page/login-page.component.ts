c6c03e27dbbd59d329995a7c616972c86e8c9506
Rusich <artem.skobar@yandex.ru>
2019-09-29 23:27:57 +0300

    add pages guard

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from '../shared/model';
 import { AuthService } from '../shared/services/auth.service';
-import { Router } from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -12,13 +12,21 @@ import { Router } from '@angular/router';
 export class LoginPageComponent implements OnInit {
   form: FormGroup;
   submitted: boolean;
+  message: string;
 
   constructor(
     public auth: AuthService,
     private router: Router,
+    private route: ActivatedRoute,
   ) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['loginAgain']) {
+        this.message = 'Залогинься снова бро!';
+      }
+    });
+
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.required, Validators.email,
