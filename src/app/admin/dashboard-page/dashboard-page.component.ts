2f5ef07fef5337463c21c9f463d4a18ef71cd684
Rusich <artem.skobar@yandex.ru>
2019-10-01 07:47:26 +0300

    add posts list page

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostService} from "../shared/post.service";
+import {Post} from "../shared/model";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss'],
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  pSub: Subscription;
 
-  // constructor() { }
+  constructor(private postServise: PostService) { }
+
+  remove(id: string) {
+    console.log(id);
+  }
 
   ngOnInit() {
+    this.pSub = this.postServise.getAll().subscribe(posts => {
+      this.posts = posts;
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.pSub) {
+      this.pSub.unsubscribe();
+    }
   }
 
 }
