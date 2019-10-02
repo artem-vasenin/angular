3638cfe33aefecc93d6b2e9b393afa990e5e3848
Rusich <artem.skobar@yandex.ru>
2019-10-02 11:25:24 +0300

    add post detail page

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {PostService} from "../admin/shared/post.service";
+import {Observable} from "rxjs";
+import {Post} from "../admin/shared/model";
+import {switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-post-page',
@@ -6,10 +11,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-page.component.scss'],
 })
 export class PostPageComponent implements OnInit {
+  post$: Observable<Post>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService,
+  ) { }
 
   ngOnInit() {
+    this.post$ =  this.route.params
+      .pipe(switchMap((params: Params) => {
+        return this.postService.getById(params['id']);
+    }));
   }
 
 }
